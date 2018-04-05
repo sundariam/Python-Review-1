c61330dbe29643875832bbc34c5bc99342c02a3f
sundariam <37808047+sundariam@users.noreply.github.com>
2018-04-05 23:39:46 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,46 +1,70 @@
 import re
 import pickle
 import argparse
+
+
+def AddNewWord(currentword, nextword):
+    if currentword not in words:
+        words[currentword] = {}
+
+    i_dict = words[currentword]
+    if nextword in i_dict:
+        i_dict[nextword] += 1
+    else:
+        i_dict[nextword] = 1
+
+
+def lastword_dict(lastword, words, nextword):
+    if lastword in words:
+        special_dict = words[lastword]  # отдельно рассматриваем последнее
+        # слово в строке и его словарь
+        if nextword in special_dict:
+            special_dict[nextword] += 1
+        else:
+            special_dict[nextword] = 1
+    else:
+        words[lastword] = {}
+        special_dict = words[lastword]
+        special_dict[nextword] = 1
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--input-dir", nargs='?')
 parser.add_argument("--model")
 args = parser.parse_args()
+
+words = {}
+lastword = ''
+
 if args.input_dir is not None:
-    f = open(args.input_dir)
+    with open(args.input_dir) as f:
+        for line in f:
+            result = re.findall(r'\w+', line)
+            linesize = len(result)
+            if linesize == 0:
+                break
+
+            for i in range(linesize - 1):
+                AddNewWord(result[i], result[i + 1])
+
+            lastword_dict(lastword, words, result[0])
+
+            lastword = result[linesize - 1]
+
 else:
     f = input()
-d = {}
-lastword = ''
-for line in f:
-    result = re.findall(r'\w+', line)
-    n = len(result)
-    if n == 0:
-        break
-    lend = len(d)
-    for i in range(n - 1):
-        if result[i] in d:
-            r = d[result[i]]
-        else:
-            d[result[i]] = {}
+    for line in f:
+        result = re.findall(r'\w+', line)
+        linesize = len(result)
+        if linesize == 0:
+            break
 
-        t = d[result[i]]
-        if result[i + 1] in t:
-            t[result[i + 1]] += 1
-        else:
-            t[result[i + 1]] = 1
-    if lastword in d:
-        t = d[lastword]
-        if result[0] in t:
-            t[result[0]] += 1
-        else:
-            t[result[0]] = 1
-    else:
-        d[lastword] = {}
-        t = d[lastword]
-        t[result[0]] = 1
+        for i in range(linesize - 1):
+            AddNewWord(result[i], result[i + 1])
 
-    lastword = result[n - 1]
+        lastword_dict(lastword, words, result[0])
 
-f = open(args.model, 'wb')
-pickle.dump(d, f)
-f.close()
+        lastword = result[linesize - 1]
+
+with open(args.model, "wb") as f:
+    pickle.dump(words, f)
